d2489d1dc6770af5e588a924dfdcf4e44fbd1c5c
nezzyxd <nezzyxbotz@gmail.com>
2022-04-27 01:08:00 +0700

    RyuV3

---
+++ ryudata/sewa.js
@@ -0,0 +1,11 @@
+exports.sewa = () => {
+	return`𝗢 𝗣 𝗘 𝗡  𝗦 𝗘 𝗪 𝗔  𝗕 𝗢 𝗧
+
+• *1 Minggu : 4.000 ( 4K )*
+• *1 Bulan : 6.000 ( 6K )*
+• *Permanen : 8.000 ( 8K )*
+• *Join Murid Bot : 20.000 ( 20K )*
+
+Minat Ketik .owner
+`
+}
